d4f847ecd367a324eadd01a909ada0c4099bb07a
Hasanain-Jaafar <hassanainadm@gmail.com>
2023-10-11 21:47:20 +0200

    new Blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.{html,js}", "./dist/Blogs/*.{html,js}"],
   theme: {
     extend: {
       fontFamily:{
